a19599a6876a3348b8bcce48f70725b5625a323b
Darvand <davinchis95@gmail.com>
2025-12-27 22:31:29 -0500

    Fix: Add delay message with Snackbar in LoadingPage for improved user feedback during loading

--- src/pages/LoadingPage.tsx
+++ src/pages/LoadingPage.tsx
@@ -1,10 +1,27 @@
 import { Page } from "@/components/Page";
-import { Spinner } from "@telegram-apps/telegram-ui";
-import { FC } from "react";
+import { Snackbar, Spinner } from "@telegram-apps/telegram-ui";
+import { FC, useEffect, useState } from "react";
+import { InfoCircleOutline28 } from "tmaui/icons";
 
 export const LoadingPage: FC = () => {
+    const [showDelayMessage, setShowDelayMessage] = useState(false);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowDelayMessage(true);
+        }, 2000);
+        return () => clearTimeout(timer);
+    }, []);
     return (
         <Page back={false}>
+
+            {showDelayMessage && (
+                <Snackbar
+                    onClose={() => { }}
+                    before={<InfoCircleOutline28 />}
+                >
+                    Usamos un servicio gratuito para alojar nuestra API, por lo que en ocasiones puede tardar un poco en responder. ¡Gracias por tu paciencia!
+                </Snackbar>
+            )}
             <div style={{
                 flex: 1,
                 height: '100dvh',
